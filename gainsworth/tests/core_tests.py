199052557c9ff66836e41be70e3c700295b32f59
Jerimiah Willhite <>
2021-11-20 22:25:46 -0800

    add test to ensure cogs are loading properly

--- gainsworth/tests/core_tests.py
+++ gainsworth/tests/core_tests.py
@@ -13,3 +13,9 @@ def test_logger():
 def test_logfile():
     path = Path("gainsworth_debug.log")
     assert path.is_file()
+
+def test_cog_loading():
+    ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
+    for cog in (ROOT_DIR / "cogs").glob("!(__init__)*.py"):
+        print(f"Found cog: 'cog.{cog.name[:-3]}'")
+        bot.load_extension(f"gainsworth.cogs.{cog.name[:-3]}")
