7067ccfaf0c6d7442731dfdea7f29ae40cc71c98
Jerimiah Willhite <>
2021-11-20 23:59:16 -0800

    add enum type; add repr methods

--- gainsworth/db/crud.py
+++ gainsworth/db/crud.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from models import Base, User, Exercise
+from models import Base, Exercise, ResultType, User 
 
 from decouple import config
 from sqlalchemy import create_engine
@@ -17,13 +17,16 @@ if __name__ == "__main__":
     recreate_database()
     ses = Session()
     name = User(name="Razzlestorm", date_created=datetime.today())
-    exercise = Exercise(name="push-ups", reps=1, latest_date=datetime.today())
+    exercise = Exercise(name="push-ups", 
+                        result=ResultType.quantity,
+                        reps=1,
+                        latest_date=datetime.today())
     ses.add(name)
     ses.add(exercise)
     ses.commit()
     print("checking if connection creates user: ")
-    print(ses.query(User).first().name)
+    print(ses.query(User).first().name, ses.query(User).first().date_created)
     print("checking if connection creates exercise: ")
-    print(ses.query(Exercise).first().name)
+    print(ses.query(Exercise).first().name, ses.query(Exercise).first().result.value)
     ses.close_all()
     recreate_database()
--- gainsworth/db/models.py
+++ gainsworth/db/models.py
@@ -1,7 +1,9 @@
+import enum
+
 from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Enum, Integer, String, Date
 
 Base = declarative_base()
 
@@ -11,6 +13,13 @@ class User(Base):
     name = Column(String, nullable=False)
     date_created = Column(Date)
 
+    def __repr__(self):
+        return f"<User(name='{self.name}', date_created='{self.date_created}')>"
+
+
+class ResultType(enum.Enum):
+    time = 'time'
+    quantity = 'quantity'
 
 
 class Exercise(Base):
@@ -18,7 +27,13 @@ class Exercise(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     reps = Column(Integer)
+    result = Column(Enum(ResultType))
     latest_date = Column(Date)
+
+    def __repr__(self):
+        return (f"<Exercise(name='{self.name}', reps='{self.reps}', 
+                result='{self.result}', latest_date='{self.latest_date}')>")
+
 
 
 
