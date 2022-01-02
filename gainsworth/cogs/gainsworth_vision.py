46ce30994a604f27e5999422f775145b18918ad9
Jerimiah Willhite <32030231+razzlestorm@users.noreply.github.com>
2022-01-01 21:56:27 -0800

    Bugfix line cumsum (#12)
    * add groupby functions to get correct line vals
    
    * flake8

--- gainsworth/cogs/gainsworth_vision.py
+++ gainsworth/cogs/gainsworth_vision.py
@@ -5,11 +5,10 @@ import sys
 
 import discord
 from discord.ext import commands
-import numpy as np
 import pandas as pd
 import plotly.express as px
 
-from gainsworth.db.models import Exercise, User
+from gainsworth.db.models import Exercise
 
 
 class GainsVision(commands.Cog):
@@ -57,11 +56,11 @@ class GainsVision(commands.Cog):
     async def see_gains(self, ctx, time="week", plot_type="line"):
         """
         Use this command to create a visualization of all your gains for the past week,
-        month, or year! Just type g!see_gains {week/month/year} {line/histogram}, and 
+        month, or year! Just type g!see_gains {week/month/year} {line/histogram}, and
         Gainsworth will create a graph that you can download and share with friends!
         An example command might look like this: \n
         g!see_gains month histogram \n
-        OR 
+        OR
         g!see_gains (defaults to weekly line graphs)
         """
         TIMES = {
@@ -74,53 +73,67 @@ class GainsVision(commands.Cog):
             ses, user = await memory._check_registered(ctx)
         if user:
             # this gets the df and filters by time
-            exercises = pd.read_sql(ses.query(Exercise).filter(Exercise.user_id == user.id).statement, ses.bind)
+            exercises = pd.read_sql(ses.query(Exercise)
+                                    .filter(Exercise.user_id == user.id)
+                                    .statement, ses.bind)
             subset = exercises[exercises['date'] > (datetime.utcnow() - timedelta(days=TIMES.get(time, 7)))]
             ses.close()
             subset = subset.set_index('date')
 
             # create empty df filled with all dates in range
-            end = datetime.utcnow().date()
+            end = datetime.utcnow()
             start = (end-timedelta(days=TIMES.get(time, 7)))
             dates = pd.date_range(start=start, end=end, freq='D')
             idx_ref = pd.DatetimeIndex(dates)
             idx_df = pd.DataFrame(index=idx_ref)
-            subset_exc = pd.merge(idx_df, subset, how='outer', left_index=True, right_index=True)
+            subset_exc = pd.merge(idx_df,
+                                  subset,
+                                  how='outer',
+                                  left_index=True,
+                                  right_index=True)
 
             # This is necessary because if the color parameter sees a NaN it panics
             subset_exc['name'] = subset_exc['name'].fillna("")
+            subset_exc['reps'] = subset_exc['reps'].fillna(-100)
             # plotting logic
             # see templates: https://plotly.com/python/templates/#theming-and-templates
             if plot_type in ["hist", "histogram", "h", "his", "hgram"]:
+                get_max = subset_exc.groupby([pd.Grouper(freq='D'), "name"]) \
+                          .sum().reset_index(level="name")
                 fig = px.histogram(subset_exc,
-                            x=subset_exc.index,
-                            y="reps",
-                            color="name",
-                            labels = {
-                                "date": "Date",
-                                "reps": "No. of Reps",
-                                "name": "Exercises:"
-                            },
-                            title="GAINS!",
-                            template="plotly_dark+xgridoff",
-                            nbins=TIMES.get(time, 7),
-                            barmode="group"
-                            )
+                                   x=subset_exc.index,
+                                   y="reps",
+                                   color="name",
+                                   labels={
+                                           "date": "Date",
+                                           "reps": "No. of Reps",
+                                           "name": "Exercises:"
+                                          },
+                                   title="GAINS!",
+                                   template="plotly_dark+xgridoff",
+                                   nbins=TIMES.get(time, 7),
+                                   barmode="group"
+                                   )
+                fig.update_layout(yaxis={"range": [0, get_max["reps"].max()]})
             else:
+                subset_exc = subset_exc.groupby([pd.Grouper(freq='D'), "name"]) \
+                             .sum().reset_index(level="name")
+                subset_exc = subset_exc.drop(["id", "user_id"], axis=1)
                 fig = px.line(subset_exc,
-                            x=subset_exc.index,
-                            y="reps",
-                            color="name",
-                            labels = {
-                                "date": "Date",
-                                "reps": "No. of Reps",
-                                "name": "Exercises:"
-                            },
-                            title="GAINS!",
-                            template="plotly_dark+xgridoff",
-                            )
+                              x=subset_exc.index,
+                              y="reps",
+                              color="name",
+                              labels={
+                                      "index": "Date",
+                                      "date": "Date",
+                                      "reps": "Daily No. of Cumulative Reps",
+                                      "name": "Exercises:"
+                                     },
+                              title="GAINS!",
+                              template="plotly_dark+xgridoff",
+                              )
                 fig.update_traces(mode="markers+lines")
-                fig.update_layout(yaxis={"range":[0, subset_exc["reps"].max()]})
+                fig.update_layout(yaxis={"range": [0, subset_exc["reps"].max()]})
             fig.write_image("exercises.png")
             with open("exercises.png", "rb") as f:
                 file = io.BytesIO(f.read())
