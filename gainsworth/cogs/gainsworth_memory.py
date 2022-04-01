27b3fec004b590e738a402dbb2aaa59f0efc7918
Jerimiah Willhite <32030231+razzlestorm@users.noreply.github.com>
2022-04-01 09:05:47 -0700

    Bugfix fix case sensitivity (#30)
    * add .lower() checks
    
    * add extra lower

--- gainsworth/cogs/gainsworth_memory.py
+++ gainsworth/cogs/gainsworth_memory.py
@@ -264,7 +264,7 @@ class GainsMemory(commands.Cog):
                 totals = []
                 if len(args) == 1 and args[0].lower() in TIMES:
                     end = datetime.utcnow()
-                    start = (end-timedelta(days=TIMES.get(args[0])))
+                    start = (end-timedelta(days=TIMES.get(args[0].lower())))
                     result = [x for x in ses.query(Exercise.name,
                                                 Exercise.unit,
                                                 func.sum(Exercise.reps))
