606cb575f850a144f5d61df28b4c538bc8a1c607
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2022-07-23 23:21:33 -0700

    add a default to recent activity column

--- alembic/versions/793142b7f546_add_delete_cascade_to_user.py
+++ alembic/versions/793142b7f546_add_delete_cascade_to_user.py
@@ -7,7 +7,7 @@ Create Date: 2022-06-12 23:16:26.108628
 """
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy.sql import func
 
 # revision identifiers, used by Alembic.
 revision = '793142b7f546'
@@ -18,8 +18,8 @@ depends_on = None
 
 def upgrade() -> None:
     op.alter_column("users", sa.ForeignKey('exercises', ondelete='CASCADE'))
-    op.add_column("users", sa.Column('last_active', sa.DateTime()))
-
+    op.add_column("users", sa.Column('last_active', sa.DateTime(), default=func.utcnow()))
+ 
 
 def downgrade() -> None:
     op.drop_column("users", "last_active")
